a3258b214b6beb67c46cbe2f5602e797ce4a3b9a
Samlehoy <136605780+retrogade1337@users.noreply.github.com>
2023-07-01 15:40:32 +0700

    big update

--- js/script.js
+++ js/script.js
@@ -16,3 +16,37 @@ function resetFilterBtns(){
     $(this).removeClass('active-filter-btn');
   });
 }
+
+                // Fungsi untuk menambahkan item ke keranjang
+                function addToCart() {
+                  var cartCountElement = document.getElementById("cartCount");
+                  var cartCount = parseInt(cartCountElement.innerHTML);
+                  cartCount++;
+                  cartCountElement.innerHTML = cartCount;
+              }
+          
+              // Fungsi untuk menambahkan item ke favorit
+              function addToFavorite() {
+                  var favCountElement = document.getElementById("favCount");
+                  var favCount = parseInt(favCountElement.innerHTML);
+                  favCount++;
+                  favCountElement.innerHTML = favCount;
+              }
+          
+              // Fungsi untuk melakukan pencarian
+              function search() {
+                  // Implementasikan logika pencarian sesuai kebutuhan Anda
+                  console.log("Melakukan pencarian...");
+              }
+          
+              // Tambahkan event listener untuk tombol keranjang
+              var cartBtn = document.getElementById("cartBtn");
+              cartBtn.addEventListener("click", addToCart);
+          
+              // Tambahkan event listener untuk tombol favorit
+              var favBtn = document.getElementById("favBtn");
+              favBtn.addEventListener("click", addToFavorite);
+          
+              // Tambahkan event listener untuk tombol pencarian
+              var searchBtn = document.getElementById("searchBtn");
+              searchBtn.addEventListener("click", search);
